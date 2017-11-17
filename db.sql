978034f9692e5302ffbd80dca96a180a7ad8bb2b
Azq2 <5139482+Azq2@users.noreply.github.com>
2017-11-17 22:36:07 +0300

    db.sql

--- db.sql
+++ db.sql
@@ -1,4 +1,87 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `vkapp` (
+  `group_id` int(10) UNSIGNED NOT NULL,
+  `token` varchar(1024) NOT NULL,
+  `handshake` varchar(256) NOT NULL,
+  `secret` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `app` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vkapp_catlist_cats` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `descr` varchar(1024) NOT NULL,
+  `photo` varchar(1024) NOT NULL,
+  `width` int(10) UNSIGNED NOT NULL,
+  `height` int(10) UNSIGNED NOT NULL,
+  `sex` tinyint(4) NOT NULL DEFAULT '0',
+  `price` decimal(10,0) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vkapp_catlist_deny` (
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `time` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vkapp_catlist_files` (
+  `md5` char(32) NOT NULL,
+  `time` int(11) NOT NULL,
+  `attach_id` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vkapp_catlist_messages` (
+  `id` varchar(64) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `vkapp_catlist_money_history` (
+  `id` int(11) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `ctime` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `diff` decimal(10,0) NOT NULL,
+  `value` decimal(10,0) NOT NULL,
+  `descr` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vkapp_catlist_reposts` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `ctime` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vkapp_catlist_users` (
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `ctime` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `mtime` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `deny` tinyint(4) NOT NULL DEFAULT '0',
+  `action` varchar(128) NOT NULL DEFAULT '',
+  `state` text,
+  `first_name` varchar(256) NOT NULL DEFAULT '',
+  `last_name` varchar(256) NOT NULL DEFAULT '',
+  `sex` smallint(6) NOT NULL,
+  `money` decimal(10,0) NOT NULL DEFAULT '0',
+  `bonus` decimal(10,0) NOT NULL DEFAULT '0',
+  `food` decimal(10,0) NOT NULL DEFAULT '0',
+  `toilet` decimal(10,0) NOT NULL DEFAULT '0',
+  `cats` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `notify` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vkapp_catlist_user_cats` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `ctime` int(10) UNSIGNED NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `photo` varchar(1024) NOT NULL,
+  `width` int(10) UNSIGNED NOT NULL,
+  `height` int(10) UNSIGNED NOT NULL,
+  `sex` tinyint(3) UNSIGNED NOT NULL,
+  `cheerfulness` int(11) NOT NULL DEFAULT '100'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `vk_comm_users` (
   `cid` int(10) UNSIGNED NOT NULL,
@@ -10,6 +93,11 @@ CREATE TABLE `vk_grabber_blacklist` (
   `source_id` varchar(64) NOT NULL,
   `source_type` varchar(32) NOT NULL,
   `remote_id` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `vk_grabber_blacklist2` (
+  `group_id` int(10) UNSIGNED NOT NULL,
+  `object` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `vk_grabber_data` (
@@ -112,17 +200,53 @@ CREATE TABLE `vk_posts_reposts` (
   `comments` int(10) UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `vk_smm_money` (
+  `group_id` int(10) UNSIGNED NOT NULL,
+  `money` decimal(12,2) NOT NULL DEFAULT '0.00',
+  `last_date` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `vk_special_posts` (
   `post_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+ALTER TABLE `vkapp`
+  ADD PRIMARY KEY (`group_id`);
+
+ALTER TABLE `vkapp_catlist_cats`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `vkapp_catlist_deny`
+  ADD PRIMARY KEY (`user_id`);
+
+ALTER TABLE `vkapp_catlist_files`
+  ADD PRIMARY KEY (`md5`);
+
+ALTER TABLE `vkapp_catlist_messages`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `vkapp_catlist_money_history`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `vkapp_catlist_reposts`
+  ADD PRIMARY KEY (`user_id`,`owner_id`,`post_id`);
+
+ALTER TABLE `vkapp_catlist_users`
+  ADD PRIMARY KEY (`user_id`);
+
+ALTER TABLE `vkapp_catlist_user_cats`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `vk_comm_users`
   ADD PRIMARY KEY (`cid`,`uid`);
 
 ALTER TABLE `vk_grabber_blacklist`
   ADD PRIMARY KEY (`group_id`,`source_id`,`source_type`,`remote_id`);
+
+ALTER TABLE `vk_grabber_blacklist2`
+  ADD PRIMARY KEY (`group_id`,`object`);
 
 ALTER TABLE `vk_grabber_data`
   ADD PRIMARY KEY (`id`);
@@ -167,10 +291,19 @@ ALTER TABLE `vk_posts_reposts`
   ADD PRIMARY KEY (`post_id`,`group_id`,`user_id`),
   ADD KEY `user_id` (`user_id`);
 
+ALTER TABLE `vk_smm_money`
+  ADD PRIMARY KEY (`group_id`);
+
 ALTER TABLE `vk_special_posts`
   ADD PRIMARY KEY (`post_id`,`group_id`);
 
 
+ALTER TABLE `vkapp_catlist_cats`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `vkapp_catlist_money_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `vkapp_catlist_user_cats`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 ALTER TABLE `vk_grabber_data`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 ALTER TABLE `vk_join_stat`
