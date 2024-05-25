9e90ffd96c3e5dc40630721fc2505c1f29a71d21
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2024-05-25 22:58:45 +0000

    fix

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,9 @@ define('APP', __DIR__."/");
 
 ob_start();
 error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+
+ini_set('display_errors', PHP_SAPI == 'cli' ? 'On' : 'Off');
+
 ini_set('memory_limit', '128M');
 ini_set('arg_separator.input', ';&');
 date_default_timezone_set('Europe/Moscow');
